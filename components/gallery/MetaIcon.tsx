51661e55f8befe62bbc2668652f4a1c40d146c14
orionmiz <x3ph3r@naver.com>
2022-03-06 12:26:49 +0900

    fix: use modified icon file names

--- components/gallery/MetaIcon.tsx
+++ components/gallery/MetaIcon.tsx
@@ -1,3 +1,4 @@
+import { getIconFileName } from "lib/gallery";
 import Image from "next/image";
 import styles from 'styles/Utils.module.css'
 
@@ -7,7 +8,7 @@ export default function MetaIcon({ variant, size }: {
 }) {
   return (
     <>
-      <Image src={`/img/icon/${variant}.svg`} width={size} height={size} className={styles.adjustTheme} alt={variant} />
+      <Image src={`/img/icon/${getIconFileName(variant)}.svg`} width={size} height={size} className={styles.adjustTheme} alt={variant} />
     </>
   )
 }
